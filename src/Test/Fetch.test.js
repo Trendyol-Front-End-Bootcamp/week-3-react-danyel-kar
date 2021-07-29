ffbb2532f05511ad77512af2ec5038b0864e077a
DANYELKAR <danyelkar1905@gmail.com>
2021-07-29 19:09:37 +0300

    Some tests are done

--- src/Test/Fetch.test.js
+++ src/Test/Fetch.test.js
@@ -1,15 +1,18 @@
-import {fetchItems, searchItems} from '../Services/Fetch'
+import {fetchItems, getFilterItems, searchItems} from '../Services/Fetch'
 
 
 describe('fetch items', ()=>{
 
-    test('check not null', async() => {
+    it('check not null', async() => {
         const result = await fetchItems()
         expect(result).not.toBeNull();
     });
 
-      test('check size of results', async() => {
+    it('check size of results', async() => {
         const result = await fetchItems()
+        // console.log(result);
+        // const info = result.info;
+        // expect(info).toHaveProperty("pages");
         const resultsLength = result.length;
         expect(resultsLength).toBe(20);
     });
@@ -17,15 +20,38 @@ describe('fetch items', ()=>{
 
 describe('search items', ()=>{
 
-    test('check not empty search bar', async() => {
+    it('check not empty search bar', async() => {
         const result = await searchItems()
         expect(result).not.toBeNull();
     });
 
-    test('check not empty search bar', async() => {
+    it('check if first item searched', async() => {
         const searchCharacter ="Rick";
         const result = await searchItems(searchCharacter)
-        expect(result[0].name).toContain(searchCharacter);
+        result.forEach(element=> {
+             expect(element.name).toContain(searchCharacter);
+        })
+    }); 
+})
+
+describe('filter items', ()=>{
+
+    it('check  for empty gender and status', async() => {
+        const result = await getFilterItems("", "")
+        expect(result).not.toBeNull();
     });
-    
+
+    it('check not empty filter status', async() => {
+        const result = await getFilterItems("Alive", "")
+        result.forEach(element => {
+            expect(element.status).toBe("Alive");
+        });
+    });
+
+    it('check not empty filter gender', async() => {
+        const result = await getFilterItems("", "Male")
+        result.forEach(element => {
+            expect(element.gender).toBe("Male");
+        });
+    });
 })
